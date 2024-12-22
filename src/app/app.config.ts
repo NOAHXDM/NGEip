ce72b2e9226c6d7ea57825a7a1a52657f5b07d5d
Wilson.Weng <l7960261@gmail.com>
2024-12-22 22:05:06 +0800

    firebase init
    https://firebase.google.com/docs/emulator-suite/install_and_configure?hl=zh&authuser=0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth, connectAuthEmulator } from '@angular/fire/auth';
+import {
+  getFirestore,
+  provideFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,16 +16,27 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'noah-eip',
-        appId: '1:596868888265:web:521cbfab9471bd2aa0ff18',
-        storageBucket: 'noah-eip.firebasestorage.app',
-        apiKey: 'AIzaSyAAfL4F-eSuDLeDrgppC-jcNqAqE5dwqKY',
-        authDomain: 'noah-eip.firebaseapp.com',
-        messagingSenderId: '596868888265',
-        measurementId: 'G-8GHHDKZNPE',
+        apiKey: 'AIzaSyBigNRXTWt2AG78OhY03LE9yq5NTgRXeJ0',
+        authDomain: 'fir-eip.firebaseapp.com',
+        projectId: 'fir-eip',
+        storageBucket: 'fir-eip.firebasestorage.app',
+        messagingSenderId: '757825775592',
+        appId: '1:757825775592:web:99c975263289ccbb52665d',
       })
     ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    // provideAuth(() => getAuth()),
+    provideAuth(() => {
+      const auth = getAuth();
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+      return auth;
+    }),
+    // provideFirestore(() => getFirestore()),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+      return firestore;
+    }),
   ],
 };
