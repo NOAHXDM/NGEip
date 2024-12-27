fe254ced9e4dc2b8a2523cf3f103d7d66dc1ee77
Wilson.Weng <l7960261@gmail.com>
2024-12-27 13:52:26 +0800

    feat: add Register component and update routing for registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { CodelabComponent } from './codelab/codelab.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard, noAuthGuard } from './guards/auth.guard';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authGuard] },
   { path: 'Login', component: LoginComponent, canActivate: [noAuthGuard] },
+  { path: 'Register', component: RegisterComponent, canActivate: [noAuthGuard]},
   { path: 'CodeLab', component: CodelabComponent },
 ];
