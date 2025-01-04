35204005db3719ef711b18520a670b8ec62f1a69
陳玟宇 <wc8183182@gmail.com>
2025-01-05 01:44:52 +0800

    圖片照片是名字首字

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss',
 })
-export class AvatarComponent {}
+export class AvatarComponent {
+  name: string = 'example'; // 初始化名字
+  email: string = 'example@gmail.com'; // 初始化邮箱
+}
