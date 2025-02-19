fb61f7f8354cc38c29e59d47523c3aa52acfca0d
陳玟宇 <wc8183182@gmail.com>
2025-02-19 17:37:38 +0800

    user-list隱藏部分

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,26 +2,14 @@ import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { User, UserService } from '../services/user.service';
 import { Observable, take } from 'rxjs';
-
-// export interface PeriodicElement {
-//   name: string;
-//   position: number;
-//   weight: number;
-//   symbol: string;
-// }
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-// ];
+import { MatIconModule } from '@angular/material/icon';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 /**
  * @title Basic use of `<table mat-table>`
@@ -31,7 +19,17 @@ import { Observable, take } from 'rxjs';
   styleUrl: './user-list.component.scss',
   templateUrl: './user-list.component.html',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class UserListComponent {
   list$: Observable<User[]>;
@@ -50,12 +48,25 @@ export class UserListComponent {
       },
     });
   }
-  displayedColumns: string[] = [
+
+  dataSource = this.userArray;
+  columnsToDisplay = [
     'name',
     'email',
-    'job-title',
-    'job-rank',
-    'wfh',
+    'jobTitle',
+    'jobRank',
+    'remainingLeaveHours',
   ];
-  // dataSource = ELEMENT_DATA;
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement?: PeriodicElement | null;
+}
+
+export interface PeriodicElement {
+  Name: string;
+  Email: string;
+  jobTitle: string;
+  JobRank: string;
+  remoteWorkEligibility: 'N/A' | 'WFH2' | 'WFH4.5';
+  remoteWorkRecommender: string[];
+  remainingLeaveHours: number;
 }
