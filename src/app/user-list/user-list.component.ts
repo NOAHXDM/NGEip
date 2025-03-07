e00d220ee2a902f130fd6c4a8383b6cae5bcf90c
Wilson.Weng <l7960261@gmail.com>
2025-03-07 21:25:57 +0800

    Refactor user list component to use async pipe and streamline date formatting

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,44 +1,30 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import { User, UserService } from '../services/user.service';
-import { Observable, take } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
-import { Timestamp } from '@angular/fire/firestore';
+import { MatChipsModule } from '@angular/material/chips';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
 import { UserNamePipe } from '../pipes/user-name.pipe';
 import { FirestoreTimestampPipe } from '../pipes/firestore-timestamp.pipe';
-import { CommonModule } from '@angular/common';
-import { MatChipsModule } from '@angular/material/chips';
-export interface PeriodicElement {
-  Name: string;
-  Email: string;
-  jobTitle: string;
-  JobRank: string;
-  remoteWorkEligibility: 'N/A' | 'WFH2' | 'WFH4.5';
-  remoteWorkRecommender: string[] /*背書人*/;
-  startDate?: Timestamp;
-  birthday?: Timestamp;
-  exitDate?: Timestamp;
-  uid: string;
-  remainingLeaveHours: number;
-}
+import { User, UserService } from '../services/user.service';
+
 @Component({
   selector: 'app-user-list',
   styleUrl: './user-list.component.scss',
   templateUrl: './user-list.component.html',
   standalone: true,
   imports: [
-    MatTableModule,
-    MatIconModule,
-    UserNamePipe,
+    AsyncPipe,
     FirestoreTimestampPipe,
-    CommonModule,
     MatChipsModule,
+    MatIconModule,
+    MatTableModule,
+    UserNamePipe,
   ],
 })
 export class UserListComponent {
   list$: Observable<User[]>;
-  userArray: User[] = [];
-
   displayedColumns: string[] = [
     'name',
     'startDate',
@@ -53,36 +39,5 @@ export class UserListComponent {
 
   constructor(private userService: UserService) {
     this.list$ = this.userService.list$;
-  }
-  ngOnInit() {
-    this.profile();
-  }
-  profile() {
-    this.list$.pipe(take(1)).subscribe({
-      next: (userList) => {
-        this.userArray = userList;
-        console.log(userList);
-      },
-    });
-  }
-
-  showStarDate(startDate: any) {
-    if (startDate instanceof Timestamp) {
-      return startDate.toDate().toLocaleDateString();
-    }
-    return 'N/A';
-  }
-  showBirthday(birthday: any) {
-    if (birthday instanceof Timestamp) {
-      return birthday.toDate().toLocaleDateString();
-    }
-    return '未輸入';
-  }
-
-  showQuitDate(exitDate: any) {
-    if (exitDate instanceof Timestamp) {
-      return exitDate.toDate().toLocaleDateString();
-    }
-    return '在職中';
   }
 }
