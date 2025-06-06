d47db5420c3cbec97214cbc5ee8e58bd555dd8da
陳玟宇 <wc8183182@gmail.com>
2025-06-06 18:04:19 +0800

    改寫法

--- tools/delete_not_use_publicID.js
+++ tools/delete_not_use_publicID.js
@@ -7,51 +7,53 @@ if (typeof process.env.CLOUDINARY_URL === "undefined") {
     "Check you've got a .env file in the root of the project with the CLOUDINARY_URL environment variable for your Cloudinary product environment."
   );
 }
-
-//*firebase上目前有使用的
-const firebaseUsed = require("./in_use_publicID.json");
-console.log(firebaseUsed);
-
 // Return "https" URLs by setting secure: true
 cloudinary.config({
   secure: true,
 });
 
-home();
+//*firebase上目前有使用的照片
+const firebaseUsed = require("./in_use_publicID.json");
+console.log("firebase目前使用的照片", firebaseUsed);
 
-//抓到所有圖片URL
-async function home() {
-  const result = await cloudinary.search
-    .expression("resource_type:image")
-    .execute();
+// 取得雲端所有上傳過的圖片
+function getAllUploadImages() {
+  return cloudinary.search.expression("resource_type:image").execute();
+}
 
-  //* 所有上傳過的PublicIds;
-  const cloudPublicIds = result.resources.map((user) => user.public_id);
-  console.log("雲端全部ID", cloudPublicIds);
-
-  const unusedOnFirebase = cloudPublicIds.filter((cloudPublicIds) => {
-    return !firebaseUsed.includes(cloudPublicIds);
-  });
-
-  if (unusedOnFirebase.length === 0) {
-    console.log("沒有可刪除的資源。");
-    return;
+// 找到未使用的照片
+findNotUsedImages();
+async function findNotUsedImages() {
+  const result = await getAllUploadImages();
+  const resources = result.resources;
+  const notUsedInFirebase = resources
+    //雲端所有上傳過的照片
+    .map((user) => user.public_id)
+    .filter((cloudPublicIds) => !firebaseUsed.includes(cloudPublicIds));
+  if (notUsedInFirebase.length === 0) {
+    return console.log("沒有可刪除的資料");
   }
+  console.log("將刪除以下未使用於 Firebase 的圖片", notUsedInFirebase);
+  confirmAndDeleteImages(notUsedInFirebase);
+}
 
-  console.log("以下是未使用於 Firebase 的圖片，是否要刪除？", unusedOnFirebase);
-
-  // 建立 readline 介面
+// 刪除照片
+function confirmAndDeleteImages(notUsedInFirebase) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-
-  rl.question("輸入 y 確認刪除，其他鍵取消: ", (answer) => {
+  rl.question("輸入 y 確認刪除，其他鍵取消: ", async (answer) => {
     if (answer === "y") {
-      cloudinary.api.delete_resources(unusedOnFirebase);
-      console.log("刪除成功");
+      try {
+        await cloudinary.api.delete_resources(notUsedInFirebase);
+        console.log("刪除成功");
+      } catch (error) {
+        console.log(" 刪除失敗，原因：", error.message);
+      }
     } else {
       console.log("已取消刪除。");
     }
+    rl.close();
   });
 }
